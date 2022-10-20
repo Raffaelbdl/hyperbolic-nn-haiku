68031614ee4c6b7aab9799ee03873ede090d5de3
Raffaelbdl <raffaelbdl@gmail.com>
2022-10-20 17:28:18 +0900

    :sparkles: Add decorator for activation

--- haiku_hnn/activation.py
+++ haiku_hnn/activation.py
@@ -5,15 +5,25 @@ from jax import numpy as jnp
 from haiku_hnn.core import expmap_zero, logmap_zero
 
 
+def with_logexpmap(f, c: float):
+    """Decorator to map functions in hyperbolic space"""
+
+    def mapper(*args, **kwargs):
+        v = logmap_zero(args[0], c)
+        v = f(v, **kwargs)
+        v = expmap_zero(v, c)
+        return v
+
+    return mapper
+
+
 def map_activation(
     x: jnp.ndarray,
     c: float,
     activation: Callable[[jnp.ndarray], jnp.ndarray],
     **kwargs,
 ) -> jnp.ndarray:
-    v = logmap_zero(x, c)
-    v = activation(v, **kwargs)
-    return expmap_zero(v, c)
+    return with_logexpmap(activation, c)(x, **kwargs)
 
 
 def map_relu(x: jnp.ndarray, c: float):
