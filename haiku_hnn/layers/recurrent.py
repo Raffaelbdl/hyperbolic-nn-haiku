0ee6356634eab9b144b109d18b0dbedacf26f6f4
Raffaelbdl <raffaelbdl@gmail.com>
2022-11-02 10:25:38 +0900

    ✨ Add stereographic rnn and gru

--- haiku_hnn/layers/recurrent.py
+++ haiku_hnn/layers/recurrent.py
@@ -6,86 +6,105 @@ from jax import numpy as jnp
 
 from haiku_hnn.activation import map_activation, map_relu
 from haiku_hnn.core import m_add, logmap_zero, m_matrix_vector_multiplication
-from haiku_hnn.layers.linear import RemappingLinear
+from haiku_hnn.core.stereographic import logmap0, m_dot
+from haiku_hnn.layers.linear import RemappingLinear, StereographicLinear
 
 
-class MappingVanillaRNN(hk.VanillaRNN):
-    """Hyperbolic VanillaRNN module with remapping
+class StereographicVanillaRNN(hk.VanillaRNN):
+    """VanillaRNN Module in K-stereographic model
 
-    based on Hyperbolic Neural Networks (http://arxiv.org/abs/1805.09112)
+    References:
+        Hyperbolic Neural Networks (http://arxiv.org/abs/1805.09112)
+
+    # TODO Change Attribute section
+    Non-inherited attributes:
+        k (float): the curvature of the manifold
     """
 
     def __init__(
         self,
         hidden_size: int,
-        c: float,
-        double_bias: bool = False,
+        k: float,
+        double_bias: bool = True,
         name: Optional[str] = None,
     ):
         super().__init__(hidden_size, double_bias, name)
-        self.c = c
+        self.k = k
 
     def __call__(self, inputs, prev_state):
-        input_to_hidden = RemappingLinear(self.hidden_size, self.c)
-        hidden_to_hidden = RemappingLinear(
-            self.hidden_size, self.c, with_bias=self.double_bias
+        """Computes a hyperbolic rnn transform of the input and the previous state"""
+        input_to_hidden = StereographicLinear(self.hidden_size, self.k)
+        hidden_to_hidden = StereographicLinear(
+            self.hidden_size, self.k, with_bias=self.double_bias
         )
+
         # arbitrary order for Möbius addition used here
-        pre_nonlinearity = m_add(hidden_to_hidden(prev_state), input_to_hidden(inputs))
-        out = map_relu(pre_nonlinearity, self.c)
+        out = m_add(hidden_to_hidden(prev_state), input_to_hidden(inputs))
+        out = map_relu(out, self.k)
         return out, out
 
 
-class MappingGRU(hk.GRU):
-    """Hyperbolic GRU module with remapping
+class StereographicGRU(hk.GRU):
+    """GRU Module in K-stereographic model
 
-    based on Hyperbolic Neural Networks (http://arxiv.org/abs/1805.09112)
+    References:
+        Hyperbolic Neural Networks (http://arxiv.org/abs/1805.09112)
+
+    # TODO Change Attribute section
+    Non-inherited attributes:
+        k (float): the curvature of the manifold
     """
 
     def __init__(
         self,
         hidden_size: int,
-        c: float,
+        k: float,
         w_i_init: Optional[hk.initializers.Initializer] = None,
         w_h_init: Optional[hk.initializers.Initializer] = None,
         b_init: Optional[hk.initializers.Initializer] = None,
         name: Optional[str] = None,
     ):
         super().__init__(hidden_size, w_i_init, w_h_init, b_init, name)
-        self.c = c
+        self.k = k
 
     def __call__(self, inputs, state):
-        # naive implementation from the paper
-        # TODO copy haiku native implementation
+        """Computes a hyperbolic gru transform of the input and the previous state"""
+        # Naive implementation from the paper
+        # TODO Copy Haiku native implementation
 
         if inputs.ndim not in (1, 2):
             raise ValueError("GRU inputs must be rank-1 or rank-2.")
 
-        r_input_to_hidden = RemappingLinear(self.hidden_size, self.c)
-        r_hidden_to_hidden = RemappingLinear(self.hidden_size, self.c, with_bias=False)
+        r_input_to_hidden = StereographicLinear(self.hidden_size, self.k)
+        r_hidden_to_hidden = StereographicLinear(
+            self.hidden_size, self.k, with_bias=False
+        )
 
-        z_input_to_hidden = RemappingLinear(self.hidden_size, self.c)
-        z_hidden_to_hidden = RemappingLinear(self.hidden_size, self.c, with_bias=False)
+        z_input_to_hidden = StereographicLinear(self.hidden_size, self.k)
+        z_hidden_to_hidden = StereographicLinear(
+            self.hidden_size, self.k, with_bias=False
+        )
 
         r = m_add(r_hidden_to_hidden(state), r_input_to_hidden(inputs))
-        r = nn.sigmoid(logmap_zero(r, self.c))
+        r = nn.sigmoid(logmap0(r, self.k))
 
         z = m_add(z_hidden_to_hidden(state), z_input_to_hidden(inputs))
-        z = nn.sigmoid(logmap_zero(z, self.c))
+        z = nn.sigmoid(logmap0(z, self.k))
 
-        h_tilt_input_to_hidden = RemappingLinear(self.hidden_size, self.c)
-        # TODO add args in get_parameter
+        h_tilt_input_to_hidden = StereographicLinear(self.hidden_size, self.k)
+        # TOTO Add args in get_parameter
         h_tilt_hidden_to_hidden = hk.get_parameter(
             "riemannian_u", [self.hidden_size, self.hidden_size]
         )
-        h_tilt_1 = m_matrix_vector_multiplication(
-            jnp.dot(h_tilt_hidden_to_hidden, jnp.diag(r)), state
-        )
+
+        # first term of the addition
+        h_tilt_1 = jnp.dot(h_tilt_hidden_to_hidden, jnp.diag(r))
+        h_tilt_1 = map_activation(h_tilt_1, self.k, nn.tanh)
+        h_tilt_1 = m_dot(state, h_tilt_1)
+        # second term of the addition
         h_tilt_2 = h_tilt_input_to_hidden(inputs)
-        h_tilt = map_activation(m_add(h_tilt_1, h_tilt_2), self.c, nn.tanh)
+        h_tilt = m_add(h_tilt_1, h_tilt_2)
 
-        h = m_add(
-            state, m_matrix_vector_multiplication(jnp.diag(z)), m_add(-state, h_tilt)
-        )
+        state = m_add(state, m_dot(m_add(-state, h_tilt), jnp.diag(z)))
 
-        return h, h
+        return state, state
