d42c4a585246539cd56c1043cd803d11da1d5df9
Raffaelbdl <raffaelbdl@gmail.com>
2022-11-07 23:27:35 +0900

    📇 Fill init

---
+++ haiku_hnn/__init__.py
@@ -0,0 +1,19 @@
+from haiku_hnn.core.stereographic import expmap0, logmap0
+
+# import layers
+from haiku_hnn.layers.linear import StereographicLinear
+from haiku_hnn.layers.recurrent import StereographicVanillaRNN
+from haiku_hnn.layers.recurrent import StereographicGRU
+
+# import optimizers
+from haiku_hnn.optimizers.alias import riemannian_adagrad
+from haiku_hnn.optimizers.alias import riemannian_adam
+from haiku_hnn.optimizers.alias import rsgd
+
+from haiku_hnn.optimizers.transform import mixed_optimizer
+from haiku_hnn.optimizers.update import apply_mixed_updates
+
+# import activation functions
+from haiku_hnn.core.activation import m_softmax
+from haiku_hnn.core.activation import m_relu
+from haiku_hnn.core.activation import m_tanh
