6a12fd8bd1551a5bf117259b5d5178a996fa5d01
Raffaelbdl <raffaelbdl@gmail.com>
2023-01-18 15:45:19 +0900

    ⚡ Add methods to stereographic

--- haiku_hnn/core/manifolds/stereographic.py
+++ haiku_hnn/core/manifolds/stereographic.py
@@ -19,7 +19,7 @@ class Stereographic(Manifold):
         raise NotImplementedError()
 
     def expmap(self, x: jnp.ndarray, u: jnp.ndarray) -> jnp.ndarray:
-        norm_u = jnp.sqrt(jnp.sum(jnp.square(u), axis=-1, keepdims=True))
+        norm_u = jnp.sqrt(jnp.sum(jnp.square(u), axis=-1, keepdims=True)) + 1e-15
         conf_factor = self.conformal_factor(x)
 
         transf_u = self._tan_k(jnp.sqrt(jnp.abs(self.k)) * conf_factor * norm_u / 2.0)
@@ -56,7 +56,7 @@ class Stereographic(Manifold):
         return jnp.where(cond, 1.0 / norm * max_norm, 1.0) * u
 
     def get_origin_like(self, u: jnp.ndarray) -> jnp.ndarray:
-        raise NotImplementedError()
+        return self.proj(self.expmap0(jnp.zeros_like(u)), 4e-3)
 
     def _mobius_add(self, u: jnp.ndarray, v: jnp.ndarray) -> jnp.ndarray:
         u2 = jnp.sum(jnp.square(u), axis=-1, keepdims=True)
@@ -83,6 +83,7 @@ class Stereographic(Manifold):
         return self._tan_k(s * self._arctan_k(norm_u)) * u / norm_u
 
     def conformal_factor(self, u: jnp.ndarray) -> jnp.ndarray:
+        u = self.proj(u, 4e-3)
         return 2.0 / (1.0 + self.k * jnp.sum(jnp.square(u), axis=-1, keepdims=True))
 
     def _tan_k(self, x: jnp.ndarray) -> jnp.ndarray:
@@ -100,3 +101,71 @@ class Stereographic(Manifold):
             return safe_arctanh(jnp.sqrt(-self.k) * x)
         else:
             return jnp.arctan(x)
+
+    def norm(self, x: jnp.ndarray, u: jnp.ndarray) -> jnp.ndarray:
+        """Computes the norm in the K-stereographic model
+
+        Reference:
+            Constant Curvature Graph Convolutional Networks
+                (https://arxiv.org/pdf/1911.05076v1.pdf)
+
+        Args:
+            x (jnp.ndarray): the point where the norm is computed
+            k (float): the curvature of the manifold
+
+            axis (int): the axis along which the sum is performed
+            keepdims (bool): if True, the axes which are reduced are left in the result as dimensions with size one
+        """
+        return self.conformal_factor(x) * jnp.sqrt(
+            jnp.sum(jnp.square(u), axis=-1, keepdims=True)
+        )
+
+    def _gyration(self, u: jnp.ndarray, v: jnp.ndarray, w: jnp.ndarray) -> jnp.ndarray:
+        """Computes the gyration vector of w by [u, v]
+
+        Reference:
+            Constant Curvature Graph Convolutional Networks
+                (https://arxiv.org/pdf/1911.05076v1.pdf)
+
+        Args:
+            u, v, w (jnp.ndarray): the points where gyr[u, v]w is computed
+            k (float): the curvature of the manifold
+
+            axis (int): the axis along which the sum is performed
+            keepdims (bool): if True, the axes which are reduced are left in the result as dimensions with size one
+        """
+        u2 = jnp.sum(jnp.square(u), axis=-1, keepdims=True)
+        v2 = jnp.sum(jnp.square(v), axis=-1, keepdims=True)
+        uv = jnp.sum(u * v, axis=-1, keepdims=True)
+        uw = jnp.sum(u * w, axis=-1, keepdims=True)
+        vw = jnp.sum(v * w, axis=-1, keepdims=True)
+        k2 = jnp.square(self.k)
+
+        a = -k2 * uw * v2 - self.k * vw + 2 * k2 * uv * vw
+        b = -k2 * vw * u2 + self.k * uw
+        d = 1 - 2 * self.k * uv + k2 * u2 * v2
+        return w + 2 * (a * u + b * v) / jnp.maximum(d, 1e-15)
+
+    def parallel_transport(
+        self, x: jnp.ndarray, y: jnp.ndarray, v: jnp.ndarray
+    ) -> jnp.ndarray:
+        """Computes the parallel transport between two tangent spaces
+
+        Reference:
+            Constant Curvature Graph Convolutional Networks
+                (https://arxiv.org/pdf/1911.05076v1.pdf)
+
+        Args:
+            x (jnp.ndarray): the first tangent space
+            y (jnp.ndarray): the second tangent space
+            v (jnp.ndarray): the point to transport
+            k (float): the curvature of the manifold
+
+        Returns:
+            The point v after parallel transport from tangent space of x to y
+        """
+        return (
+            self._gyration(y, -x, v)
+            * self.conformal_factor(x)
+            / self.conformal_factor(y)
+        )
